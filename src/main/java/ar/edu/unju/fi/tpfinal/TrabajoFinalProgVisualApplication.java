5ad938a1c33f797e365f4da413c9a39d0846803e
abel-mendez <abel.m.villagra@gmail.com>
2021-05-31 22:53:45 -0300

    commit inicial

---
+++ src/main/java/ar/edu/unju/fi/tpfinal/TrabajoFinalProgVisualApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoFinalProgVisualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoFinalProgVisualApplication.class, args);
+	}
+
+}
