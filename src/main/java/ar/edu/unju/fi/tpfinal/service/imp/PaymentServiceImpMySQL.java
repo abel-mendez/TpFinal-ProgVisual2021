88903a894cb7250e82efc9d15d874b43eef51620
abel-mendez <abel.m.villagra@gmail.com>
2021-06-07 20:56:08 -0300

    agregando imp de payment

--- src/main/java/ar/edu/unju/fi/tpfinal/service/imp/PaymentServiceImpMySQL.java
+++ src/main/java/ar/edu/unju/fi/tpfinal/service/imp/PaymentServiceImpMySQL.java
@@ -1,5 +1,45 @@
 package ar.edu.unju.fi.tpfinal.service.imp;
 
-public class PaymentServiceImpMySQL {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ar.edu.unju.fi.tpfinal.models.Payment;
+import ar.edu.unju.fi.tpfinal.reposiroty.IPaymentRepository;
+import ar.edu.unju.fi.tpfinal.service.IPaymentService;
+
+public class PaymentServiceImpMySQL implements IPaymentService {
+	private List<Payment> payments=new ArrayList<Payment>();
+	@Autowired
+	private IPaymentRepository paymentRepo;
+	@Autowired
+	Payment payment;
+	
+	@Override
+	public void guardarPayment(Payment payment) {
+		paymentRepo.save(payment);
+	}
+
+	@Override
+	public List<Payment> getAllPayments() {
+		this.payments=(List<Payment>) paymentRepo.findAll();
+		return payments;
+	}
+
+	@Override
+	public Payment getPayment() {
+		return this.payment;
+	}
+
+	@Override
+	public Payment getPaymentById(String id) {
+		Payment pay=paymentRepo.findById(id).get();
+		return pay;
+	}
+
+	@Override
+	public void deletPaymentById(String id) {
+		paymentRepo.deleteById(id);
+	}
 }
