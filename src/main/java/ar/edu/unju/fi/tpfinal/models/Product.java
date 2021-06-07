ecb7534fff753ab7a9f91eb0684be87f89733feb
Diego <Diego@DESKTOP-ASHC1O0>
2021-06-07 04:22:10 -0300

    clases creadas en el paquete models
    se creo la clase Product, ProductLine y orderDetail

---
+++ src/main/java/ar/edu/unju/fi/tpfinal/models/Product.java
@@ -0,0 +1,116 @@
+package ar.edu.unju.fi.tpfinal.models;
+
+/**
+ * @author Diego
+ *
+ */
+public class Product {
+	private String productCode;
+	private String productName;
+	private String productLine;
+	private String productScale;
+	private String productVendor;
+	private String productDescription;
+	private byte quantityInStock;
+	private double buyPrice;
+	private double msrp;
+	//CONSTRUCTORES
+	/**
+	 * Constructor por defecto
+	 */
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * Constructor parametrizado
+	 * 
+	 * @param productCode
+	 * @param productName
+	 * @param productLine
+	 * @param productScale
+	 * @param productVendor
+	 * @param productDescription
+	 * @param quantityInStock
+	 * @param buyPrice
+	 * @param msrp
+	 */
+	public Product(String productCode, String productName, String productLine, String productScale,
+			String productVendor, String productDescription, byte quantityInStock, double buyPrice, double msrp) {
+		super();
+		this.productCode = productCode;
+		this.productName = productName;
+		this.productLine = productLine;
+		this.productScale = productScale;
+		this.productVendor = productVendor;
+		this.productDescription = productDescription;
+		this.quantityInStock = quantityInStock;
+		this.buyPrice = buyPrice;
+		this.msrp = msrp;
+	}
+	//SETERs y GETERs
+	public String getProductCode() {
+		return productCode;
+	}
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductLine() {
+		return productLine;
+	}
+	public void setProductLine(String productLine) {
+		this.productLine = productLine;
+	}
+	public String getProductScale() {
+		return productScale;
+	}
+	public void setProductScale(String productScale) {
+		this.productScale = productScale;
+	}
+	public String getProductVendor() {
+		return productVendor;
+	}
+	public void setProductVendor(String productVendor) {
+		this.productVendor = productVendor;
+	}
+	public String getProductDescription() {
+		return productDescription;
+	}
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	public byte getQuantityInStock() {
+		return quantityInStock;
+	}
+	public void setQuantityInStock(byte quantityInStock) {
+		this.quantityInStock = quantityInStock;
+	}
+	public double getBuyPrice() {
+		return buyPrice;
+	}
+	public void setBuyPrice(double buyPrice) {
+		this.buyPrice = buyPrice;
+	}
+	public double getMsrp() {
+		return msrp;
+	}
+	public void setMsrp(double msrp) {
+		this.msrp = msrp;
+	}
+	//TO STRING
+	@Override
+	public String toString() {
+		return "Product [productCode=" + productCode + ", productName=" + productName + ", productLine=" + productLine
+				+ ", productScale=" + productScale + ", productVendor=" + productVendor + ", productDescription="
+				+ productDescription + ", quantityInStock=" + quantityInStock + ", buyPrice=" + buyPrice + ", msrp="
+				+ msrp + "]";
+	}
+	
+
+}
