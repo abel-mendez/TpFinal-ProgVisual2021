e4986e454964a64ae40beb20f6830bde0493200d
abel-mendez <abel.m.villagra@gmail.com>
2021-06-07 18:47:34 -0300

    agregando imp de employee

--- src/main/java/ar/edu/unju/fi/tpfinal/service/imp/EmployeeServiceImpMySQL.java
+++ src/main/java/ar/edu/unju/fi/tpfinal/service/imp/EmployeeServiceImpMySQL.java
@@ -1,5 +1,47 @@
 package ar.edu.unju.fi.tpfinal.service.imp;
 
-public class EmployeeServiceImpMySQL {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ar.edu.unju.fi.tpfinal.models.Employee;
+import ar.edu.unju.fi.tpfinal.reposiroty.IEmployeRepository;
+import ar.edu.unju.fi.tpfinal.service.IEmployeeService;
+
+
+public class EmployeeServiceImpMySQL implements IEmployeeService {
+
+	private List<Employee> employees=new ArrayList<Employee>();
+	@Autowired
+	private IEmployeRepository employeeRepo;
+	@Autowired
+	Employee employee;
+	
+	@Override
+	public void guardarEmployee(Employee employee) {
+		employeeRepo.save(employee);
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		this.employees=(List<Employee>) employeeRepo.findAll();
+		return employees;
+	}
+
+	@Override
+	public Employee getEmployee() {
+		return this.employee;
+	}
+
+	@Override
+	public Employee getEmployeeById(int id) {
+		Employee emp=employeeRepo.findById(id).get();
+		return emp;
+	}
+
+	@Override
+	public void deletEmployeeById(int id) {
+		employeeRepo.deleteById(id);
+	}
 }
