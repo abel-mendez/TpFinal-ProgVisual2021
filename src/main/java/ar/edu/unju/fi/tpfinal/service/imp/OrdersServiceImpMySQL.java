049b017fef39bba9db01a2004f66d8f0732ccaf8
LEANDRO VICTORINO CRUZ <83726994+Leo-apu@users.noreply.github.com>
2021-06-11 10:56:11 -0300

    Creando implementadores para Customers y Orders.

---
+++ src/main/java/ar/edu/unju/fi/tpfinal/service/imp/OrdersServiceImpMySQL.java
@@ -0,0 +1,47 @@
+package ar.edu.unju.fi.tpfinal.service.imp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ar.edu.unju.fi.tpfinal.models.Orders;
+import ar.edu.unju.fi.tpfinal.reposiroty.IOrdersRepository;
+import ar.edu.unju.fi.tpfinal.service.IOrdersService;
+
+public class OrdersServiceImpMySQL implements IOrdersService{
+	
+	private List<Orders> orderss= new ArrayList<Orders>();
+	@Autowired
+	private IOrdersRepository orderRepo;
+	@Autowired
+	Orders orders;
+
+	@Override
+	public void guardarOrder(Orders order) {
+		orderRepo.save(order);
+	}
+
+	@Override
+	public List<Orders> getAllOrders() {
+		this.orderss=(List<Orders>) orderRepo.findAll();
+		return orderss;
+	}
+
+	@Override
+	public Orders getOrder() {
+		return this.orders;
+	}
+
+	@Override
+	public Orders getOrderById(int id) {
+		Orders ord= orderRepo.findById(id).get();
+		return ord;
+	}
+
+	@Override
+	public void deleteOrderById(int id) {
+		orderRepo.deleteById(id);
+	}
+
+}
