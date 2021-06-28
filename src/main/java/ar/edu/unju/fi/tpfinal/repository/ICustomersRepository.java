8efcc2230d1d36cf25f05696107d79c464afd490
LEANDRO VICTORINO CRUZ <83726994+Leo-apu@users.noreply.github.com>
2021-06-28 03:37:13 -0300

    Documentacion Service,ServiceImp y Repository de Customers y Employee.

--- src/main/java/ar/edu/unju/fi/tpfinal/repository/ICustomersRepository.java
+++ src/main/java/ar/edu/unju/fi/tpfinal/repository/ICustomersRepository.java
@@ -4,6 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 
 import ar.edu.unju.fi.tpfinal.models.Customers;
 
+/**
+ * Interface ICustomersRepository
+ * Interface que sirve para enlazar a la base datos y utilizar operaciones
+ * basicas del Crud.
+ * @author ProgVisual2021
+ *
+ */
 public interface ICustomersRepository extends CrudRepository<Customers, Integer>{
 
 }
--- src/main/java/ar/edu/unju/fi/tpfinal/service/IEmployeeService.java
+++ src/main/java/ar/edu/unju/fi/tpfinal/service/IEmployeeService.java
@@ -3,13 +3,37 @@ package ar.edu.unju.fi.tpfinal.service;
 import java.util.List;
 
 import ar.edu.unju.fi.tpfinal.models.Employee;
-
-
-
+/**
+ * Interface de Servicio IEmployeeService
+ * que nos permite definir los metodos que podemos utilizar.
+ * @author ProgVisual2021
+ *
+ */
 public interface IEmployeeService {
+	/**
+	 * Metodo que nos permite guardar un Employee a la base de datos
+	 * @param employee parametro que tiene el objeto a guardar.
+	 */
 	public void guardarEmployee(Employee employee);
+	/**
+	 * Metodo que muestra la lista de Customers que tenemos en la base de datos
+	 * @return retorna una lista de Customers
+	 */
 	public List<Employee> getAllEmployees();
+	/**
+	 * Metodo que Obtiene un Employee
+	 * @return retorna la lista de Employees
+	 */
 	public Employee getEmployee();
+	/**
+	 * Metodo que Busca un Employee por id
+	 * @param id parametro que se usa para obtener un Employee por su id
+	 * @return retorna un Employee que correspone al parametro id
+	 */
 	public Employee getEmployeeById(int id);
+	/**
+	 * Metodo que elimina un Employee idetificado por el id
+	 * @param id parametro que sirve para identidicar al Employee a eliminar
+	 */
 	public void deletEmployeeById(int id);
 }
